f34c114e303cee9f31fd32b174800e0b94c16c92
Loma Gerhold <loma.gerhold@go.sfcollege.edu>
2025-03-19 00:33:58 +0100

    Update settings.py

--- accounting_system/settings.py
+++ accounting_system/settings.py
@@ -10,7 +10,7 @@ SECRET_KEY = 'django-insecure-key-replace-in-production'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [lamindz.pythonanywhere.com', '127.0.0.1']
 
 # Application definition
 INSTALLED_APPS = [
